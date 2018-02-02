f5e2255bfc7f630c69d30c076e6c9eab9265797a
Pat-M <pmcphe2@gmail.com>
2018-02-01 18:42:04 -0600

    moved js scripts to separate directory, removed unnecessary comments

---
+++ js/mainScripts.js
@@ -0,0 +1,43 @@
+$( "button#jQueryColorChange" ).click(function() {
+  $( ":button" ).removeClass( "selected" );
+  $(this).toggleClass( "selected" );
+});
+
+
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove
+  // the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    filterRemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) filterAddClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function filterAddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function filterRemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
